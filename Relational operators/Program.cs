5f8012fdb18ec3df8d76a0e7d3b90b898c2f56b8
jacam666 <ja6cam@gmail.com>
2021-06-27 19:53:20 +0100

    Add project files.

---
+++ Relational operators/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Relational_operators
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Please enter your age : ");
+            int age = Convert.ToInt32(Console.ReadLine());
+            if (age <= 19)
+            {
+                Console.WriteLine("You qualify, Please take your kindle");
+            }
+            else
+            {
+                Console.WriteLine("Damn so unlucky, you are way to old. YOU GET NOTHING!!!!!");
+            }
+        }
+    }
+}
